7f5e9ffe5dbd1fd828a4d65bbed395831f3f004d
murraygi <133593977+murraygi@users.noreply.github.com>
2024-10-31 10:17:47 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Cheese</li>
+      <li>Hummus</li>
+      <li>Meat</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
