16f11084aae91b4d5f763719a925365b4e08bf69
murraygi <133593977+murraygi@users.noreply.github.com>
2024-12-04 09:09:24 +0000

    notes app strike through practice

---
+++ notes-app-strike-through-notes/src/components/ToDoItem.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prevValue) => {
+      return !prevValue;
+    });
+  }
+
+  return (
+    <li
+      onClick={handleClick}
+      style={{ textDecoration: isDone ? "line-through" : "none" }}
+    >
+      {props.text}
+    </li>
+  );
+}
+
+export default ToDoItem;
